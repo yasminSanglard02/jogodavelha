dcde964a9e486b5104f2d0c3c8416e95e442c2fa
Yasmin Sanglard Xavier <yasmin.xavier@aluno.faeterj-brm.faetec.rj.gov.br>
2025-09-29 15:40:48 -0300

    esta pronto

--- jogodaVelha2.js
+++ jogodaVelha2.js
@@ -1,13 +1,15 @@
 const readlineSync = require('readline-sync');
 
-let tabuleiro = [" "," "," "," "," "," "," "," "," "]
+let tabuleiro = [" "," "," "," "," "," "," "," "," "];
 let jogador = "X";
 let jogadas = 0;
 
-function jogo() {
+function mostrarTabuleiro() {
 console.log(`
     ${tabuleiro[0]} | ${tabuleiro[1]} | ${tabuleiro[2]}
+    ----------
     ${tabuleiro[3]} | ${tabuleiro[4]} | ${tabuleiro[5]} 
+    ----------
     ${tabuleiro[6]} | ${tabuleiro[7]} | ${tabuleiro[8]} 
 `);
 }
@@ -23,8 +25,8 @@ function verificarVitoria() {
 }
 do {
     mostrarTabuleiro();
-    let pos = readlineSync.questionInt(`Jogador ${jogador}, escolha uma posição (1-9): `) - 1;
-    
+    let pos = 
+    readlineSync.questionInt(`Jogador ${jogador}, escolha uma posicao (1-9): `) - 1;
     if (tabuleiro[pos] === " ") {
         tabuleiro[pos] = jogador;
         jogadas++;
